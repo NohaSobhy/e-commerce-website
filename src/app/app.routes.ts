6cd10faf6f59b6a91c98f496d4fb3879a2a11a8a
NohaSobhy <noha.sobhi2001@gmail.com>
2025-03-09 00:44:15 +0200

    update to angular version 17

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
-export const routes: Routes = [
-    { 
-        path: 'products', 
-        component: ProductsPageComponent, 
-        title: "Amazon | All Productsg"
-    },
-];
+export const routes: Routes = [];
