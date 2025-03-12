1413ed18868071e1576e97202bcee18e8c12e60a
NohaSobhy <noha.sobhi2001@gmail.com>
2025-03-12 09:42:54 +0200

    add furniture page

--- src/app/pages/products/furniture/furniture.component.ts
+++ src/app/pages/products/furniture/furniture.component.ts
@@ -3,22 +3,29 @@ import { ProductsService } from '../../../services/products.service';
 import { ProductCardComponent } from '../../../components/product-card/product-card.component';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../../models/product.model';
+import { SidebarComponent } from '../../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-furniture',
   standalone: true,
-  imports: [ProductCardComponent, CommonModule],
+  imports: [ProductCardComponent, CommonModule, SidebarComponent],
   templateUrl: './furniture.component.html',
   styleUrl: './furniture.component.css'
 })
 export class FurnitureComponent {
   products: Product[] = [];
+  filteredProducts: any[] = [];
 
   constructor(private productService: ProductsService) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data: Product[]) => {
-      this.products = data.filter((product: Product) => product.category === 'furniture');
+      this.products = data.filter((product: Product) => product.category === 'Furniture');
+      this.filteredProducts = [...this.products];
     });
+  }
+
+  updateFilteredProducts(filtered: any[]) {
+    this.filteredProducts = filtered;
   }
 }
